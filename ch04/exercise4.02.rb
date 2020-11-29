dbca6d18a49a63376c0c3549994c9a8503e709a6
rjp-at-insight <robert.patterson@insight.com>
2020-11-29 16:13:40 -0500

    started exercise 4.02

---
+++ ch04/exercise4.02.rb
@@ -0,0 +1,35 @@
+#   AUTHOR      : Robert James Patterson
+#   DATE        : 11/29/2020
+#   FILE        : exercise4.02.rb
+#   SYNOPSIS    : using methods in Ruby.
+
+# In this exercise, we will write a method that emulates an API client that can take any number 
+# of parameters. Additionally, we will call the method using the single splat operator.
+#
+# A common idiom is to assemble arguments in this way if there are conditions determining which 
+# arguments to pass to the method. Let's take a look at an example using an API client that makes 
+# a web request. The web request method can take any number of arguments:
+
+
+# First, we need to create a method that implements a method called get_data and takes the url, 
+# headers, and params variables, which use the splat operator.
+def get_data(url, headers = {}, *params)
+    
+    puts "Calling #{url}"
+    
+    if headers.length > 0
+        puts "Headers: #{headers}"
+    else
+        puts "No headers"
+    end
+    
+    params.each do |param|
+        puts "Found param: #{param}"
+    end
+
+end
+
+# Next , we write a method that assembles parameters into an array.
+def assemble_params(include_headers=false, include_data_in_search=false, only_show_my_record=true)
+
+end
